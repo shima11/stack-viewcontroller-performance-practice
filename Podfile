b18d5aa9ab20a4922e6b2d72683288fb9673228c
Shima <shima.jin@icloud.com>
2020-10-11 17:53:51 +0900

    add podfile

---
+++ Podfile
@@ -0,0 +1,8 @@
+
+target 'presented-viewcontroller-performance-practice' do
+
+  use_frameworks!
+
+  pod 'Reveal-SDK', :configurations => ['Debug']
+  
+end
---
+++ Pods/Target Support Files/Pods-presented-viewcontroller-performance-practice/Pods-presented-viewcontroller-performance-practice-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_presented_viewcontroller_performance_practice : NSObject
+@end
+@implementation PodsDummy_Pods_presented_viewcontroller_performance_practice
+@end
